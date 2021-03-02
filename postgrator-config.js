a982e30fb52011f22381ca4770f963a5a07bc042
dimitrycodes <strongafetyking@yahoo.com>
2021-03-02 23:30:18 +0530

    Fix issue

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
 if (process.env.NODE_ENV === "production") {
-  const pg = require('pg');
-  pg.defaults.ssl = { rejectUnauthorized: false };
+    const pg = require('pg');
+    pg.defaults.ssl = { rejectUnauthorized: false };
 }
 
 module.exports = {
-  'migrationsDirectory': 'migrations',
-  'driver': 'pg',
-  'connectionString': (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL
+    'migrationsDirectory': 'migrations',
+    'driver': 'pg',
+    'connectionString': (process.env.NODE_ENV === 'test') ?
+        process.env.TEST_DATABASE_URL :
+        process.env.DATABASE_URL
 };
